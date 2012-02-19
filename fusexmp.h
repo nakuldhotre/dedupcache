6746603bd5f4708e998aa0e924a587ec0401c1ea
Ranjan Sarpangala Venkatesh <ranjan@ranjan-HP-EliteBook-8440p.(none)>
2012-02-18 21:32:36 -0800

    using_uthash

--- fusexmp.h
+++ fusexmp.h
@@ -1,23 +1,22 @@
 
-struct entry
+typedef struct 
 {
 	unsigned char key[20];
-        uint32_t hash_key;
+
 	int index;
-};
+        UT_hash_handle hh;
+}HASH_TABLE_INODE_N_BLOCK;
 
-struct blistnode
+
+typedef struct 
 {
-	struct entry *curr;
-	struct entry *next;
-};
+	int index;	
+	uint32_t hash_key;
+        unsigned char data_block[4096];
+        UT_hash_handle hh;
+}HASH_TABLE_MD5_CACHE;
 
-
-struct cache_entry
-{
-	unsigned char data_block[4096];
-	struct entry *hash2;
-	struct blistnode *block_list;
-	int ref_count;
-};
+HASH_TABLE_INODE_N_BLOCK *block_inode=NULL;
+HASH_TABLE_MD5_CACHE *md5=NULL;
+static int count_block_inode=0,count_md5_cache=0;
 
